7b6a07aafa7ebf6a27999bb10da41848fa2a080a
ZakariaElKhaldi <zkariaelkhaldi@gmail.com>
2026-04-10 09:42:32 +0100

    fix: set client_class=OAuth2Client on social login views

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,11 +1,14 @@
 from allauth.socialaccount.providers.github.views import GitHubOAuth2Adapter
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from dj_rest_auth.registration.views import SocialLoginView
 
 
 class GitHubLogin(SocialLoginView):
     adapter_class = GitHubOAuth2Adapter
+    client_class = OAuth2Client
 
 
 class GoogleLogin(SocialLoginView):
     adapter_class = GoogleOAuth2Adapter
+    client_class = OAuth2Client
